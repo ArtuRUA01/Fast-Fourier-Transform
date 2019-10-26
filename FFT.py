1b87b6bbe7f1350e5fc13c3f6842ef1639eeabed
ArtuR <44754904+ArtuRUA01@users.noreply.github.com>
2019-10-26 14:38:36 +0300

    Program FFT Python3 v2
    Added to the program:
    1) random numbers with a range
    2) numbers are written to a file

--- FFT.py
+++ FFT.py
@@ -5,13 +5,14 @@
 
 
 import numpy as np
+import random
 
 
 # In[100]:
 
 
 def DFT(x):
-    x = np.asarray(x, dtype = float)
+    x = np.asarray(x, dtype=float)
     N = len(x)
     f = []
     for i in range(N):
@@ -30,26 +31,38 @@ def FFT(x):
     X_even = DFT(x[::2])
     X_odd = DFT(x[1::2])
     factor = np.exp(-2j * np.pi * np.arange(N) / N)
-#     print(X_even)
-#     print(X_odd)
-#     print(factor)
-    
-#     print(X_even + factor[:int(N/2)] * X_odd)
-#     print(X_even + factor[int(N/2):N] * X_odd)
-    
-    return np.concatenate([X_even + factor[:int(N/2)] * X_odd, X_even + factor[int(N/2):N] * X_odd])
+    #     print(X_even)
+    #     print(X_odd)
+    #     print(factor)
+    #     print(X_even + factor[:int(N/2)] * X_odd)
+    #     print(X_even + factor[int(N/2):N] * X_odd)
+
+    return np.concatenate([X_even + factor[:int(N / 2)] * X_odd, X_even + factor[int(N / 2):N] * X_odd])
 
 
 # In[102]:
+size = int(input('Enter size of array: '))
+if size % 2 != 0:
+    print('Size must be even number')
+    exit()
+diapason_min = int(input('Enter min number on diapason: '))
+diapason_max = int(input('Enter max number on diapason: '))
+x = []
+inputfile = 'NumbersFFT.txt'
+myfile = open(inputfile, mode='w')
 
+for i in range(size):
+    r = random.randint(diapason_min, diapason_max)
+    myfile.write(str(r) + '\n')
+myfile.close()
 
-x = np.array([4,2,1,4,6,3,5,2])
+myfile = open(inputfile, mode='r')
+
+for i in myfile:
+    x.append(int(i))
+
+x = np.array(x)
+
 print(FFT(x))
 print(np.fft.fft(x))
-
-
-# In[ ]:
-
-
-
-
+myfile.close()
